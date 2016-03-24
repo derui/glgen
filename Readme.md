2522dcaaab9d4a05bc00c4c3debff8cee553f52c
derui <derutakayu@gmail.com>
2016-03-24 22:22:40 +0900

    Fix enums printing not contained any group

--- Readme.md
+++ Readme.md
@@ -17,3 +17,15 @@ $ omake install
 ## Use library
 This library providing OpenGL bindings, but not contains all functions of OpenGL 3.2/3.2 and between 4.0 to 4.5 .
 
+Provided libraries in this repository are as **library** , not **package** , so sample using this library is follows for OCaml source for OpenGL 3.2 .
+
+```ocaml
+(* Compiling this source must link ogl_3_2.cma/cmxa file *)
+(* Ogl_<major>_<minor> module contains functions OpenGL defined *)
+module Ogl = Ogl_3_2
+(* Ogl_enum_<major>_<minor> module contains enums OpenGL defined *)
+module Ogl_enum = Ogl_enum_3_2
+```
+
+You can use multi version OpenGL library version in one program with link multi version of it, but should not use multi version.
+Functions and enums defined by OpenGL has backward compatibility each version (some versions has breaking changes), then you decide the version of library provided to use in a program.
--- gen/gen_enum.ml
+++ gen/gen_enum.ml
@@ -6,21 +6,29 @@ open Core.Std
 module Type_map = Map.Make(String)
 module Type_set = Set.Make(String)
 
+let print_enum ppf enums v =
+  let enum = Type_map.find_exn enums v in
+  let name = String.lowercase enum.Enum.name in 
+  match enum.Enum.value with
+  | `Int32 v -> Format.fprintf ppf "@,@[let %s@ =@ %ldl@]" name v
+  | `Int64 v -> Format.fprintf ppf "@,@[let %s@ =@ %LdL@]" name v
+
 let generate ppf enums groups =
-  Type_map.iteri groups ~f:(fun ~key ~data ->
-    let name = Util.normalize_name key
-                               |> Fn.flip String.drop_prefix 1 
-                               |> String.capitalize in 
+  let group_contained = Type_set.empty in 
+  let group_contained = Type_map.fold groups ~f:(fun ~key ~data printed ->
+    let name = Util.normalize_name key |> Fn.flip String.drop_prefix 1 |> String.capitalize in 
     Format.fprintf ppf "@[<v 2>module %s = struct" name;
 
-    Type_set.filter data ~f:(Type_map.mem enums) |>
-        Type_set.iter ~f:(fun v ->
-          let enum = Type_map.find_exn enums v in
-          let name = String.lowercase enum.Enum.name in 
-          match enum.Enum.value with
-          | `Int32 v -> Format.fprintf ppf "@,@[let %s@ =@ %ld@]" name v
-          | `Int64 v -> Format.fprintf ppf "@,@[let %s@ =@ %Ld@]" name v
-        );
+    let contained = Type_set.filter data ~f:(Type_map.mem enums) in
+    Type_set.iter contained ~f:(print_enum ppf enums);
 
-    Format.fprintf ppf "@]@,@[<v 0>end@]@."
-  )
+    Format.fprintf ppf "@]@,@[<v 0>end@]@.";
+    Type_set.union printed contained
+  ) ~init:group_contained in
+  Format.fprintf ppf "@[<v 0>";
+
+  (* Print enums on toplevel if not contained any group. *)
+  Type_map.filteri enums ~f:(fun ~key ~data -> Fn.non (Type_set.mem group_contained) key)
+  |> Type_map.iteri ~f:(fun ~key ~data -> print_enum ppf enums key);
+
+  Format.fprintf ppf "@."
