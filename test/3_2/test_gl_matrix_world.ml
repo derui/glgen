3aa758014549ba729772de8e93b6c9c06c88c08f
derui <derutakayu@gmail.com>
2016-04-12 22:07:43 +0900

    Fix world transform test

--- test/3_2/test_gl_matrix_world.ml
+++ test/3_2/test_gl_matrix_world.ml
@@ -13,12 +13,12 @@
    f ();
    Init.quit ()
 
- let index_data = Bigarray.Array1.of_array Bigarray.int Bigarray.c_layout
+ let index_data = Bigarray.Array1.of_array Bigarray.int32 Bigarray.c_layout
    [|
-     0;3;1;
-     1;3;2;
-     2;3;0;
-     0;1;2;
+     0l;3l;1l;
+     1l;3l;2l;
+     2l;3l;0l;
+     0l;1l;2l;
    |]
 
  let vertex_data = Bigarray.Array1.of_array Bigarray.float32 Bigarray.c_layout
@@ -29,9 +29,9 @@
    |]
 
  let world_trans ~scale ~trans ~rotate =
-   let perspective = Ogl.Std.Camera.make_perspective_projection ~fov:30.0 ~ratio:(640.0 /. 480.0) ~near:1.0 ~far:1000.0 in
+   (* let perspective = Ogl.Std.Camera.make_perspective_projection ~fov:30.0 ~ratio:(640.0 /. 480.0) ~near:1.0 ~far:1000.0 in *)
    let open Typedvec.Std.Algebra.Mat.Open in
-   perspective *: trans *: rotate *: scale
+   trans *: rotate *: scale
 
  let rotation x y z =
    let module M = Typedvec.Std.Algebra.Mat in 
@@ -53,7 +53,7 @@
    M.set rotate_x ~row:2 ~col:1 ~v:(sin x);
    M.set rotate_x ~row:2 ~col:2 ~v:(cos x);
    let open M.Open in
-   rotate_z *: rotate_y *: rotate_x
+   rotate_x *: rotate_y *: rotate_z
 
  let make_world scale =
    let module M = Typedvec.Std.Algebra.Mat in 
@@ -63,11 +63,11 @@
    M.set ~row:2 ~col:2 ~v:(sin scale *. 0.1) scale_t;
 
    let pos = Ogl.Std.Util.identity_mat4 () in
-   M.set ~row:0 ~col:3 ~v:(sin scale) pos;
-   M.set ~row:1 ~col:3 ~v:(sin scale) pos;
-   M.set ~row:2 ~col:3 ~v:(sin scale) pos;
+   M.set ~row:0 ~col:3 ~v:(sin scale *. 0.1) pos;
+   M.set ~row:1 ~col:3 ~v:(sin scale *. 0.1) pos;
+   M.set ~row:2 ~col:3 ~v:(sin scale *. 0.1) pos;
 
-   let angle = (sin scale *. 90.0) in
+   let angle = (sin scale) in
    let rotate = rotation angle angle angle in
 
    world_trans ~scale:scale_t ~trans:pos ~rotate
@@ -82,7 +82,7 @@
      ~data:vertex_data ~usage:Ogl_enum.gl_static_draw;
    buffer
 
- let vertex_shader_src = "
+ let vertex_shader_src = "\
 #version 330 core\n
 layout(location = 0) in vec3 VertexPosition;\n
 uniform mat4 gWorld; \n
@@ -92,7 +92,7 @@ void main () {\n
   Color = vec4(clamp(VertexPosition, 0.0, 1.0), 1.0);\n
 }\n"
 
- let fragment_shader_src = "
+ let fragment_shader_src = "\
 #version 330 core\n
 in vec4 Color;\n
 out vec4 FragColor;\n
@@ -100,7 +100,7 @@ void main() {\n
   FragColor = Color;\n
 }\n"
 
- let%spec "Ogl can apply uniform to location" =
+ let%spec "Ogl can draw elements with index elements" =
    let open Flags in
    with_sdl (fun () ->
      let open Types.Result.Monad_infix in
@@ -113,13 +113,17 @@ void main() {\n
          let open Ogl_command in
          let module A = Bigarray.Array1 in
          let sprog = Test_util.load_shaders ~vertex_shader:vertex_shader_src ~fragment_shader:fragment_shader_src in
-         let vertex_pos = get_attrib_location ~program:sprog ~name:"VertexPosition" |> Int32.of_int_exn in
-         let uniform_location = get_uniform_location ~program:sprog ~name:"gWorld" in
-         uniform_location [@ne (-1)];
-
          let arrays = A.of_array Bigarray.int32 Bigarray.c_layout [|0l|] in
          gen_vertex_arrays ~n:1 ~arrays;
          bind_vertex_array ~array:(A.get arrays 0);
+
+         let pos = get_attrib_location ~program:sprog ~name:"VertexPosition" |> Int32.of_int_exn in
+         let uniform_location = get_uniform_location ~program:sprog ~name:"gWorld" in
+         uniform_location [@ne (-1)];
+
+         let vbobj = make_vbo () in
+         use_program sprog;
+         bind_buffer ~target:Ogl_enum.gl_array_buffer ~buffer:(A.get vbobj 0);
 
          let ibo = A.of_array Bigarray.int32 Bigarray.c_layout [|0l|] in
          gen_buffers ~n:1 ~buffers:ibo;
@@ -129,11 +133,6 @@ void main() {\n
            ~size:(Bigarray.Array1.dim index_data * (Ctypes.sizeof Ctypes.uint))
            ~data:index_data ~usage:Ogl_enum.gl_static_draw;
 
-         let vbobj = make_vbo () in
-         use_program sprog;
-         enable_vertex_attrib_array ~index:vertex_pos;
-         bind_buffer ~target:Ogl_enum.gl_array_buffer ~buffer:(A.get vbobj 0);
-
          let rec loop counter scale =
            if counter = 0 then ()
            else 
@@ -141,29 +140,37 @@ void main() {\n
                clear_color ~red:0.0 ~blue:0.4 ~green:0.0 ~alpha:0.0;
                clear ~mask:Int32.(bit_or Ogl_enum.Clear_buffer_mask.gl_color_buffer_bit Ogl_enum.Clear_buffer_mask.gl_depth_buffer_bit);
 
-               vertex_attrib_pointer ~index:vertex_pos ~size:3 ~typ:Ogl_enum.Vertex_pointer_type.gl_float
-                 ~normalized:false ~stride:0 ~pointer:None;
+               enable_vertex_attrib_array ~index:pos;
 
                let m = make_world scale |> M.to_array |> Array.to_list |> Array.concat in
                let m = A.of_array Bigarray.float32 Bigarray.c_layout m in
                uniform_matrix4fv ~location:uniform_location ~count:1
                  ~transpose:true ~value:m;
+
+               bind_buffer ~target:Ogl_enum.gl_array_buffer ~buffer:(A.get vbobj 0);
+               vertex_attrib_pointer ~index:pos ~size:3 ~typ:Ogl_enum.Vertex_pointer_type.gl_float
+                 ~normalized:false ~stride:0 ~pointer:None;
+
+               bind_buffer ~buffer:ibo_buf ~target:Ogl_enum.gl_element_array_buffer;
+
                draw_elements ~mode:Ogl_enum.Primitive_type.gl_triangles ~count:12
                  ~typ:Ogl_enum.List_name_type.gl_unsigned_int ~indices:None;
 
+               disable_vertex_attrib_array ~index:pos;
+
                Gl.swap_window window;
                Timer.delay 16l;
-               loop (pred counter) (scale +. 0.001)
+               loop (pred counter) (scale +. 0.01)
              end
          in
-         loop 100 (-0.5);
+         loop 100 0.1;
 
          delete_vertex_arrays ~n:1 ~arrays;
+         delete_buffers ~n:1 ~buffers:ibo;
          delete_buffers ~n:1 ~buffers:vbobj;
        end;
        Types.Result.return ctx
        >>= Gl.delete_context) |> ignore;
      Window.destroy window |> Types.Result.return
    )
-
 ]
